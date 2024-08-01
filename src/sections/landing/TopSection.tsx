68393b02249dd63856e8d65e13ecf6cff3ee0776
config.json <configjson@proton.me>
2024-08-01 14:42:11 +0300

    fix: action area on nav/hero

--- src/sections/landing/TopSection.tsx
+++ src/sections/landing/TopSection.tsx
@@ -17,33 +17,35 @@ export default function TopSection() {
   const user = auth?.user
 
   return (
-    <div className="w-full h-screen px-9 flex items-center gap-[72px] bg-background justify-center">
-      <div className="flex flex-col gap-12">
-        <div className="flex flex-col gap-6 md:w-min">
-          <div className={cn("text-5xl md:w-max", TiroTelugu.className)}>
-            Does your conversion rate suck?
+    <div className="w-full h-screen px-9 bg-background flex justify-center">
+      <div className="flex items-center gap-[72px] max-w-[1200px]">
+        <div className="flex flex-col gap-12">
+          <div className="flex flex-col gap-6 md:w-min">
+            <div className={cn("text-5xl md:w-max", TiroTelugu.className)}>
+              Does your conversion rate suck?
+            </div>
+            <div className="flex flex-col gap-3 font-medium w-full">
+              <p>
+                This is your number one UX feedback and suggestions software to
+                skyrocket your website’s conversion rate.
+              </p>
+              <p>Create an account and become a beta tester at no cost.</p>
+            </div>
           </div>
-          <div className="flex flex-col gap-3 font-medium w-full">
-            <p>
-              This is your number one UX feedback and suggestions software to
-              skyrocket your website’s conversion rate.
-            </p>
-            <p>Create an account and become a beta tester at no cost.</p>
+          <div className="flex gap-6">
+            <Link href="/#about-us">
+              <Button bg={"white"}>Learn more</Button>
+            </Link>
+            <Link href="/account/signup">
+              <Button bg={"gradient"}>
+                {!user ? "Try it out": "Go to the app"}
+              </Button>
+            </Link>
           </div>
         </div>
-        <div className="flex gap-6">
-          <Link href="/#about-us">
-            <Button bg={"white"}>Learn more</Button>
-          </Link>
-          <Link href="/account/signup">
-            <Button bg={"gradient"}>
-              {!user ? "Try it out": "Go to the app"}
-            </Button>
-          </Link>
+        <div className="w-full hidden xl:block max-w-xl">
+          <Image src="/usellLandingImg.png" alt="landing img" width={512} height={512} className="w-full"/>
         </div>
-      </div>
-      <div className="w-full hidden xl:block max-w-xl">
-        <Image src="/usellLandingImg.png" alt="landing img" width={512} height={512} className="w-full"/>
       </div>
     </div>
   );
