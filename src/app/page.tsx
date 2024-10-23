d6c2e3bba06d3f658983ee4d0e71d247d6baa8c1
setPedro <pedross4789@gmail.com>
2024-10-23 10:26:21 +0200

    remove endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,38 @@
-"use client";
+'use client'
 
-import NavBar from "@/sections/landing/NavBar";
-import AboutUs from "@/sections/landing/AboutUs";
-import TopSection from "@/sections/landing/TopSection";
-import FirstSection from "@/sections/landing/FirstSection";
-import LastSection from "@/sections/landing/LastSection";
-import { useEffect, useRef } from "react";
-import useHash from "@/hooks/useHash";
-import Footer from "@/sections/landing/Footer";
-import HowToUse from "@/sections/landing/HowToUse";
+import NavBar from '@/sections/landing/NavBar'
+import AboutUs from '@/sections/landing/AboutUs'
+import TopSection from '@/sections/landing/TopSection'
+import FirstSection from '@/sections/landing/FirstSection'
+import LastSection from '@/sections/landing/LastSection'
+import { useEffect, useRef } from 'react'
+import useHash from '@/hooks/useHash'
+import Footer from '@/sections/landing/Footer'
+import HowToUse from '@/sections/landing/HowToUse'
 
 export default function Home() {
-  const aboutUsRef = useRef<HTMLDivElement>(null);
-  const howToUSeRef = useRef<HTMLDivElement>(null);
-  const lastSectionRef = useRef<HTMLDivElement>(null);
-  const footerRef = useRef<HTMLDivElement>(null);
-  const hash = useHash();
+  const aboutUsRef = useRef<HTMLDivElement>(null)
+  const howToUSeRef = useRef<HTMLDivElement>(null)
+  const lastSectionRef = useRef<HTMLDivElement>(null)
+  const footerRef = useRef<HTMLDivElement>(null)
+  const hash = useHash()
 
   useEffect(() => {
     switch (hash) {
-      case "#about-us":
-        aboutUsRef.current?.scrollIntoView({ behavior: "smooth" });
-        break;
-      case "#how-to-use":
-        howToUSeRef.current?.scrollIntoView({ behavior: "smooth" });
-        break;
-      case "#our-solution":
-        lastSectionRef.current?.scrollIntoView({ behavior: "smooth" });
-        break;
-      case "#contact":
-        footerRef.current?.scrollIntoView({ behavior: "smooth" });
-        break;
+      case '#about-us':
+        aboutUsRef.current?.scrollIntoView({ behavior: 'smooth' })
+        break
+      case '#how-to-use':
+        howToUSeRef.current?.scrollIntoView({ behavior: 'smooth' })
+        break
+      case '#our-solution':
+        lastSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
+        break
+      case '#contact':
+        footerRef.current?.scrollIntoView({ behavior: 'smooth' })
+        break
     }
-  }, [hash]);
+  }, [hash])
 
   return (
     <main className="flex flex-col h-screen">
@@ -56,5 +56,5 @@ export default function Home() {
         <Footer />
       </div>
     </main>
-  );
+  )
 }
